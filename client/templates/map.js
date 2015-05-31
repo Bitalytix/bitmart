38ccdb7804323b2d9cbbfad82388b713b591ef8c
Justin Maat <justin.maat@wendys.com>
2015-05-31 14:26:18 -0400

    reverting back to initial working

--- client/templates/map.js
+++ client/templates/map.js
@@ -1,5 +1,6 @@
 Meteor.subscribe('markers');
 
+function createMarker();
 
 Template.map.onCreated(function () {
     GoogleMaps.ready('mymap', function (map) {
@@ -112,3 +113,23 @@ Template.first.helpers({
         return 'buddy';
     }
 });
+
+var stationList = [
+    {"latlng":[35.681382,139.766084],name:"Tokyo Station"},
+    {"latlng":[35.630152,139.74044],name:"Shinagawa Station"},
+    {"latlng":[35.507456,139.617585],name:"Shin-Yokohama Station"},
+    {"latlng":[35.25642,139.154904],name:"Odawara Station"},
+    {"latlng":[35.103217,139.07776],name:"Atami Station"},
+    {"latlng":[35.127152,138.910627],name:"Mishima Station"},
+    {"latlng":[35.142015,138.663382],name:"Shin-Fuji Station"},
+    {"latlng":[34.97171,138.38884],name:"Shizuoka Station"},
+    {"latlng":[34.769758,138.014928],name:"Kakegawa Station"},
+    {"latlng":[34.703741,137.734442],name:"Hamamatsu Station"},
+    {"latlng":[34.762811,137.381651],name:"Toyohashi Station"},
+    {"latlng":[34.96897,137.060662],name:"Mikawa-Anjyo Station"},
+    {"latlng":[35.170694,136.881637],name:"Nagoya Station"},
+    {"latlng":[35.315705,136.685593],name:"Gifu-Hashima Station"},
+    {"latlng":[35.314188,136.290488],name:"Yonehara Station"},
+    {"latlng":[34.985458,135.757755],name:"Kyoto Station"},
+    {"latlng":[34.73348,135.500109],name:"Shin-Osaka Station"}
+];
