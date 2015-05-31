19784c21655481927c7788a7cc56a44eecf39057
Justin Maat <justin.maat@wendys.com>
2015-05-31 14:04:11 -0400

    adding more functionality to dynamic map

--- client/templates/map.js
+++ client/templates/map.js
@@ -1,30 +1,14 @@
-//Markers = new Mongo.Collection('markers');
 Meteor.subscribe('markers');
 
-var centerLocation;
-
-Template.searchbox
 
 Template.map.onCreated(function () {
     GoogleMaps.ready('mymap', function (map) {
-        google.maps.event.addListener(map.instance, 'click', function (event) {
-            Markers.insert({lat: event.latLng.lat(), lng: event.latLng.lng()});
-        });
+        
+        //google.maps.event.addListener(map.instance, 'click', function (event) {
+        //    Markers.insert({lat: event.latLng.lat(), lng: event.latLng.lng()});
+        //});
 
         var markers = {};
-
-        //var image = {
-        //    url: 'http://placehold.it/32',
-        //    // This marker is 20 pixels wide by 32 pixels tall.
-        //    size: new google.maps.Size(20, 32),
-        //    // The origin for this image is 0,0.
-        //    origin: new google.maps.Point(0,0),
-        //    // The anchor for this image is the base of the flagpole at 0,32.
-        //    anchor: new google.maps.Point(0, 32)
-        //};
-
-        console.log(GoogleMaps.maps.mymap.instance);
-        console.log('centerLocation: ' + centerLocation);
 
         function CenterControl(controlDiv, map) {
 
@@ -51,11 +35,10 @@ Template.map.onCreated(function () {
             controlText.style.paddingRight = '5px';
             controlUI.appendChild(controlText);
 
-            // Setup the click event listeners: simply set the map to
-            // Chicago
-            //google.maps.event.addDomListener(controlUI, 'click', function() {
-            //    map.setCenter(centerLocation)
-            //});
+            // Setup the click event listeners: simply set the map to Chicago
+            google.maps.event.addDomListener(controlUI, 'click', function() {
+                map.setCenter(new google.maps.LatLng(41.8337329, -87.7321555));
+            });
 
         };
 
@@ -65,6 +48,7 @@ Template.map.onCreated(function () {
 
         centerControlDiv.index = 1;
         map.instance.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(centerControlDiv);
+
 
         Markers.find().observe({
             added: function (document) {
@@ -77,22 +61,22 @@ Template.map.onCreated(function () {
                     id: document._id
                 });
 
-                //console.log(JSON.stringify(marker));
-
                 google.maps.event.addListener(marker, 'dragend', function (event) {
                     Markers.update(marker.id, {$set: {lat: event.latLng.lat(), lng: event.latLng.lng()}});
+                });
+
+                google.maps.event.addListener(marker, 'click', function (event) {
+                    console.log('clicked');
+                    console.log(event);
+                    //Markers.update(marker.id, {$set: {lat: event.latLng.lat(), lng: event.latLng.lng()}});
                 });
 
                 markers[document._id] = marker;
             },
             changed: function (newDocument, oldDocument) {
-                console.log('was changed..');
-
                 markers[newDocument._id].setPosition({lat: newDocument.lat, lng: newDocument.lng});
             },
             removed: function (oldDocument) {
-                console.log('was removed..');
-
                 markers[oldDocument._id].setMap(null);
                 google.maps.event.clearInstanceListeners(markers[oldDocument._id]);
                 delete markers[oldDocument._id];
@@ -105,57 +89,26 @@ Meteor.startup(function () {
     GoogleMaps.load();
 });
 
-//function(){
-//
-//    console.log('loaded');
-//}
 
 Template.map.helpers({
     mapOptions: function () {
         if (GoogleMaps.loaded()) {
-            centerLocation = new google.maps.LatLng(19.9325862, 7.5140332);
-
             return {
-                center: centerLocation,
+                center: new google.maps.LatLng(19.9325862, 7.5140332),
                 zoom: 3
             };
         }
+    },
+    parentProfile: function (){
+        return 'in parent';
     }
 });
 
-
-
-//function CenterControl(controlDiv, map) {
-//
-//    // Set CSS for the control border
-//    var controlUI = document.createElement('div');
-//    controlUI.style.backgroundColor = '#fff';
-//    controlUI.style.border = '2px solid #fff';
-//    controlUI.style.borderRadius = '3px';
-//    controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
-//    controlUI.style.cursor = 'pointer';
-//    controlUI.style.marginBottom = '22px';
-//    controlUI.style.textAlign = 'center';
-//    controlUI.title = 'Click to recenter the map';
-//    controlDiv.appendChild(controlUI);
-//
-//    // Set CSS for the control interior
-//    var controlText = document.createElement('div');
-//    controlText.style.color = 'rgb(25,25,25)';
-//    controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
-//    controlText.style.fontSize = '16px';
-//    controlText.style.lineHeight = '38px';
-//    controlText.style.paddingLeft = '5px';
-//    controlText.style.paddingRight = '5px';
-//    controlText.innerHTML = 'Center Map';
-//    controlUI.appendChild(controlText);
-//
-//    // Setup the click event listeners: simply set the map to
-//    // Chicago
-//    google.maps.event.addDomListener(controlUI, 'click', function() {
-//        map.setCenter(chicago)
-//    });
-//
-//};
-
-//center: new google.maps.LatLng(39.98691,-83.044943),
+Template.first.helpers({
+    profile: function(parentContext) {
+        console.log(this);
+        console.log(parentContext);
+        //console.log(parentContext);
+        return 'buddy';
+    }
+});
